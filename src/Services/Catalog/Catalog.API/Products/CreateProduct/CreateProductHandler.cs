cfd550381503926c4a292772ba91fff6bbc84c69
who1-dev <caranayjohn21.personal@gmail.com>
2026-01-12 16:11:39 -0500

    Added docker compose for postgresql documentdb and added api for getbyid and getall products

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,13 +1,10 @@
-﻿using BuildingBlocks.CQRS;
-using Catalog.API.Models;
-
-namespace Catalog.API.Products.CreateProduct;
+﻿namespace Catalog.API.Products.CreateProduct;
 
 public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
     : ICommand<CreateProductResult>;
 public record CreateProductResult(Guid Id);
 
-internal class CreateProductHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+internal class CreateProductHandler(IDocumentSession session) : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
     public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
     {
@@ -24,8 +21,10 @@ internal class CreateProductHandler : ICommandHandler<CreateProductCommand, Crea
 
         // TODO
         //save to database
-        //return result
+        session.Store(product);
+        await session.SaveChangesAsync(cancellationToken);
 
+        //return result
         return new CreateProductResult(Guid.NewGuid());
     }
 
