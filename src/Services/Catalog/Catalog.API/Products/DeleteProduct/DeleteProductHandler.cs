d9ff67c7d64959c704b6d42608311c69282f232e
who1-dev <caranayjohn21.personal@gmail.com>
2026-01-15 23:04:02 -0500

    Implemented Update and Delete Product functionality

---
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductHandler.cs
@@ -0,0 +1,16 @@
+﻿namespace Catalog.API.Products.DeleteProduct;
+
+public record DeleteProductCommand(Guid Id) : ICommand<DeleteProductResult>;
+public record DeleteProductResult(bool IsSuccess);
+
+internal class DeleteProductHandler(IDocumentSession session) :
+    ICommandHandler<DeleteProductCommand, DeleteProductResult>
+{
+    public async Task<DeleteProductResult> Handle(DeleteProductCommand command, CancellationToken cancellationToken)
+    {
+        session.Delete<Product>(command.Id);
+        await session.SaveChangesAsync(cancellationToken);
+
+        return new DeleteProductResult(true);
+    }
+}
