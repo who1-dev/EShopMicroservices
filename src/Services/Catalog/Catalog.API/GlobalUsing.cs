4c6c0a128d533bf3239fd114570a557538906f27
who1-dev <caranayjohn21.student@gmail.com>
2026-01-22 15:16:11 -0500

    Dockerize catalog api and added it to docker-compose orchestration

--- src/Services/Catalog/Catalog.API/GlobalUsing.cs
+++ src/Services/Catalog/Catalog.API/GlobalUsing.cs
@@ -6,6 +6,12 @@ global using Marten.Pagination;
 global using FluentValidation;
 global using BuildingBlocks.Behaviors;
 global using BuildingBlocks.Exceptions;
+global using BuildingBlocks.Exceptions.Handler;
 global using BuildingBlocks.CQRS;
+global using Catalog.API.Data;
 global using Catalog.API.Exceptions;
 global using Catalog.API.Models;
+
+
+
+
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,7 +1,7 @@
 
 
-using BuildingBlocks.Exceptions.Handler;
-using Catalog.API.Data;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -29,11 +29,21 @@ if (builder.Environment.IsDevelopment())
 
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
+builder.Services.AddHealthChecks()
+    .AddNpgSql(builder.Configuration.GetConnectionString("Database")!);
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline
 app.MapCarter();
 
 app.UseExceptionHandler(options => { });
+
+app.UseHealthChecks("/health",
+    new HealthCheckOptions
+    {
+        ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+    });
+
 
 app.Run();
