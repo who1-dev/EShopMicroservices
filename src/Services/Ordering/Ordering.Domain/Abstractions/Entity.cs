86d208a8a225a79434d9b1697ac2724e7117681a
who1-dev <caranayjohn21.student@gmail.com>
2026-05-05 22:39:26 -0400

    Implemented the Domain layer of CLEAN Architecture

---
+++ src/Services/Ordering/Ordering.Domain/Abstractions/Entity.cs
@@ -0,0 +1,10 @@
+﻿namespace Ordering.Domain.Abstractions;
+
+public abstract class Entity<T> : IEntity<T>
+{
+    public T Id { get; set; }
+    public DateTime? CreatedAt { get; set; }
+    public string? CreadtedBy { get; set; }
+    public DateTime? LastModified { get; set; }
+    public DateTime? LastModifiedBy { get; set; }
+}
---
+++ src/Services/Ordering/Ordering.Domain/Abstractions/IEntity.cs
@@ -0,0 +1,14 @@
+﻿namespace Ordering.Domain.Abstractions;
+
+public interface IEntity<T> : IEntity
+{
+    public T Id { get; set; }
+}
+
+public interface IEntity
+{
+    public DateTime? CreatedAt { get; set; }
+    public string? CreadtedBy { get; set; }
+    public DateTime? LastModified { get; set; }
+    public DateTime? LastModifiedBy { get; set; }
+}
