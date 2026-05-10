16d5f85980d2fb8f7aa5ed8f202db16499ecbf5e
who1-dev <caranayjohn21.student@gmail.com>
2026-05-09 21:50:15 -0400

    Completed Application layer and added MassTransit implementation in BuildingBlocks

---
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrdersByCustomer/GetOrdersByCustomerHandler.cs
@@ -0,0 +1,20 @@
+﻿
+
+namespace Ordering.Application.Orders.Queries.GetOrdersByCustomer;
+
+public class GetOrdersByCustomerHandler(IApplicationContext dbContext) :
+    IQueryHandler<GetOrdersByCustomerQuery, GetOrdersByCustomerResult>
+{
+    public async Task<GetOrdersByCustomerResult> Handle(GetOrdersByCustomerQuery query, CancellationToken cancellationToken)
+    {
+
+        var orders = await dbContext.Orders
+            .Include(x => x.OrderItems)
+            .AsNoTracking()
+            .Where(x => x.CustomerId == CustomerId.Of(query.CustomerId))
+            .OrderBy(x => x.OrderName.Value)
+            .ToListAsync(cancellationToken);
+
+        return new GetOrdersByCustomerResult(orders.ToOrderDtoList());
+    }
+}
---
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrdersByName/GetOdersByNameHandler.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Ordering.Application.Orders.Queries.GetOrdersByName;
+
+public class GetOdersByNameHandler(IApplicationContext dbContext)
+    : IQueryHandler<GetOrdersByNameQuery, GetOrdersByNameResult>
+{
+    public async Task<GetOrdersByNameResult> Handle(GetOrdersByNameQuery query, CancellationToken cancellationToken)
+    {
+        var orders = await dbContext.Orders
+            .Include(x => x.OrderItems)
+            .AsNoTracking()
+            .Where(x => x.OrderName.Value.Contains(query.Name))
+            .OrderBy(x => x.OrderName.Value)
+            .ToListAsync(cancellationToken);
+
+        return new GetOrdersByNameResult(orders.ToOrderDtoList());
+    }
+}
